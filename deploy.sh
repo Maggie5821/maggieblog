12e4711113cfba5013d93d77ed7534ad732ce1dd
Maggie5821 <312762007@qq.com>
2020-04-08 23:28:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Maggie5821/maggieblog.git master:gh-pages
 
-git remote add origin https://github.com/Maggie5821/reactinput.git
+git remote add origin https://github.com/Maggie5821/confirmcomponent.git
 
 git pull --rebase origin master
 
